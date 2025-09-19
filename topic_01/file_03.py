c76ad1daf4b2622cb12262a779d609d87478b603
mrlinuks <maxg4357@gmail.com>
2025-09-19 09:08:45 +0300

    changed topic 01

--- topic_01/file_03.py
+++ topic_01/file_03.py
@@ -1,5 +1,12 @@
 a = float(input("A num: "))
 b = float(input("B num: "))
 c = float(input("C num: "))
-ds = b**2-4*a*c
-print(ds)
+
+
+def ds(a, b, c):
+    n = b**2-4*a*c
+    return n
+
+
+d = ds(a, b, c)
+print(d)
