73425ff6873573670e0e3ae61b606dd39542b85e
mrlinuks <maxg4357@gmail.com>
2025-11-26 14:28:42 +0200

    update all

---
+++ Labs/Lab_02/Lab_02.py
@@ -0,0 +1,152 @@
+import csv
+
+# -----------<For test>-----------#
+
+
+def add_student(list, name, phone, email, group):
+    newItem = {"name": name, "phone": phone, "email": email, "group": group}
+    insertPosition = 0
+    for item in list:
+        if name > item["name"]:
+            insertPosition += 1
+        else:
+            break
+    list.insert(insertPosition, newItem)
+
+
+def delete_student(list, name):
+    for i, item in enumerate(list):
+        if item["name"] == name:
+            del list[i]
+            return True
+    return False
+
+# -----------<Main>-----------#
+
+
+def printAllList(list):
+    for elem in list:
+        strForPrint = "Student name is " + \
+            elem["name"] + ",  Phone is " + elem["phone"] + \
+            ",  Email is " + elem["email"] + ",  Group is " + elem["group"]
+        print(strForPrint)
+
+
+def addNewElement(list):
+    name = input("Pease enter student name: ")
+    phone = input("Please enter student phone: ")
+    email = input("Please enter student email: ")
+    group = input("Please enter student group: ")
+
+    newItem = {"name": name, "phone": phone, "email": email, "group": group}
+    # find insert position
+    insertPosition = 0
+    for item in list:
+        if name > item["name"]:
+            insertPosition += 1
+        else:
+            break
+    list.insert(insertPosition, newItem)
+    print("New element has been added")
+    return
+
+
+def deleteElement(list):
+    name = input("Please enter name to be delated: ")
+    deletePosition = -1
+    for item in list:
+        if name == item["name"]:
+            deletePosition = list.index(item)
+            break
+    if deletePosition == -1:
+        print("Element was not found")
+    else:
+        print("Dele position " + str(deletePosition))
+        del list[deletePosition]
+    return
+
+
+def updateElement(list):
+    name = input("Please enter name to be updated data: ")
+    for i, item in enumerate(list):
+        if item["name"] == name:
+            upname = input("Enter new name: ")
+            upphone = input("Enter new phone: ")
+            upemail = input("Enter new email: ")
+            upgroup = input("Enter new group: ")
+
+            updateItem = {"name": upname, "phone": upphone,
+                          "email": upemail, "group": upgroup}
+            del list[i]
+
+            insertPosition = 0
+            for n in list:
+                if upname > n["name"]:
+                    insertPosition += 1
+                else:
+                    break
+            list.insert(insertPosition, updateItem)
+            print("Element update")
+            return
+
+    print("Element was not found")
+    return
+
+# ---------------------------------------------------
+
+
+def readDataFromFile(studentList):
+    with open("Labs/Lab_02/Lab_02.csv") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            studentList.append(
+                {"name": row["Studname"], "phone": row["Phone"], "email": row["Gmail"], "group": row["Group"]})
+
+
+def saveData(studlist):
+    with open("Labs/Lab_02/Lab_02.csv", "w", newline="") as file:
+        fieldnames = ["Studname", "Phone", "Gmail", "Group"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in studlist:
+            writer.writerow({
+                "Studname": row["name"],
+                "Phone":    row["phone"],
+                "Gmail":    row["email"],
+                "Group":    row["group"]
+            })
+
+# ---------------------------------------------------
+
+
+def main():
+
+    student_list = []
+    readDataFromFile(student_list)
+    while True:
+        chouse = input(
+            "Please specify the action [ C create, U update, D delete, P print,  X exit ] ")
+        match chouse:
+            case "C" | "c":
+                print("New element will be created:")
+                addNewElement(student_list)
+                printAllList(student_list)
+            case "U" | "u":
+                print("Existing element will be updated")
+                updateElement(student_list)
+            case "D" | "d":
+                print("Element will be deleted")
+                deleteElement(student_list)
+            case "P" | "p":
+                print("List will be printed")
+                printAllList(student_list)
+            case "X" | "x":
+                print("Saved data and Exit")
+                saveData(student_list)
+                break
+            case _:
+                print("Wrong chouse")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Labs/Lab_02/Test_Lab_02.py
@@ -0,0 +1,30 @@
+import unittest
+import Lab_02
+
+
+class TestStudentSystem(unittest.TestCase):
+
+    def setUp(self):
+        self.list = []
+
+    def test_add_student(self):
+        student = {
+            "name": "Bob",
+            "phone": "0631234567",
+            "email": "damsbob@gmail.com",
+            "group": "kb-242"}
+        Lab_02.add_student(self.list, "Bob", "0631234567",
+                           "damsbob@gmail.com", "kb-242")
+        assert len(self.list) == 1
+        assert self.list[0], student
+
+    def test_delete_student(self):
+        self.list = [{"name": "Bob", "phone": "0631234567",
+                      "email": "damsbob@gmail.com", "group": "kb-242"}]
+        result = Lab_02.delete_student(self.list, "Bob")
+        assert result, True
+        assert len(self.list) == 0
+
+
+if __name__ == '__main__':
+    unittest.main()
